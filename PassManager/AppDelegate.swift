454e946ab94abb583309fa1e084951a4e2bcca67
Stanisław Uss <uss.stanislaw@me.com>
2015-11-14 15:44:53 +0100

    Initial Commit

---
+++ PassManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PassManager
+//
+//  Created by Stanisław Uss on 14.11.2015.
+//  Copyright © 2015 Stanisław Uss. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PassManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PassManager
+//
+//  Created by Stanisław Uss on 14.11.2015.
+//  Copyright © 2015 Stanisław Uss. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
